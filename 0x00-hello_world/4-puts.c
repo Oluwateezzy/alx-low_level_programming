7763ec7a267bc30fbff6aefac6cb1939d2b95f48
oluwatobiloba <oluwateezzy03@gmail.com>
2022-10-14 00:07:28 -0700

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
+
 /**
- * main - function that print a statement using puts function
- * return - zero if exited properly
+ * main - Entry point
+ *
+ * Return: zero if exited properly
+ *
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
