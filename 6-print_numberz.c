20489016a476b1a3577b2aa71114286ddaad46b0
oluwatobiloba <oluwateezzy03@gmail.com>
2022-10-14 07:13:01 -0700

    char num

---
+++ 6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 = success
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 48; num < 58; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
