bf866210ac23e37cf2c6338bfd215473553d4ee2
oluwatobiloba <oluwateezzy03@gmail.com>
2022-10-14 05:26:40 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 = success
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while(ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	return (0);
+}
