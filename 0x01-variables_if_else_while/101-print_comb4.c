a513326b2809d3d3a4473e9301adb60fbf9855a0
oluwatobiloba <oluwateezzy03@gmail.com>
2022-10-16 13:13:53 -0700

    once

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 = success
+ */
+int main(void)
+{
+	int num, num2, num3;
+
+	for (num = 48; num < 58; num++)
+	{
+		for (num2 = 48; num2 <58; num2++)
+		{
+			for (num3 = 48; num3 < 58; num3++)
+			{
+				if (num3 > num2 && num2 > num)
+				{
+					putchar(num);
+					putchar(num2);
+					putchar(num3);
+					if (num != 55 || num2 != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
