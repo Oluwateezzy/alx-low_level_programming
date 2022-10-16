830a20085de1e8260efb22a358ccdeec7575b321
oluwatobiloba <oluwateezzy03@gmail.com>
2022-10-16 12:35:22 -0700

    worked

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,19 +8,14 @@ int main(void)
 {
 	int num, num2;
 
-	for (num = 0; num <= 9; num++)
+	for (num = 48; num < 58; num++)
 	{
-
-		for (num2 = 0; num <= 9; num2++)
+		for (num2 = 49; num2 < 58; num2++)
 		{
-			putchar((num % 10) + '0');
-			putchar((num2 % 10) + '0');
-
-			if ((num == 9) && (num2 == 9))
-			{
-				continue;
-			}
-
+			putchar(num);
+			putchar(num2);
+			if (num == 57 && num2 == 56)
+				break;
 			putchar(',');
 			putchar(' ');
 		}
