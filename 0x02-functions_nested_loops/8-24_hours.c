bfbc6a93c91203d98fd6be3f8130ab7a672aaa1b
oluwatobiloba <oluwateezzy03@gmail.com>
2022-10-18 16:11:20 -0700

    jack bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * jack_bauer - time function
+ *
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int hour, min;
+
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (min = 0; min < 60; min++)
+		{
+			_putchar((hour / 10) + 48);
+			_putchar((hour % 10) + 48);
+			_putchar(':');
+			_putchar((min / 10) + 48);
+			_putchar((min % 10) + 48);
+			_putchar('\n');
+		}
+	}
+}
---
+++ 0x02-functions_nested_loops/8-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	jack_bauer();
+	return (0);
+}
