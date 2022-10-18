a9a7b866621631dfdb538707a7d0910172663210
oluwatobiloba <oluwateezzy03@gmail.com>
2022-10-18 16:49:49 -0700

    work

---
+++ 0x02-functions_nested_loops/9-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Return: zero
+ */
+int main(void)
+{
+	times_table();
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * times_table - multiplication table
+ *
+ * Return: void
+ */
+void times_table(void)
+{
+	int r, c, mult;
+
+	for (r = 0; r < 10; r++)
+	{
+		for (c = 0; c < 10; c++)
+		{
+			mult = r * c;
+
+			if (c == 0)
+			{
+				_putchar('0');
+			}
+			else if (mult < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(mult + 48);
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((mult / 10) + 48);
+				_putchar((mult % 10) + 48);
+			}
+		}
+		_putchar('\n');
+	}
+}
