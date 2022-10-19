502bb06eaf33f12d65bb857e8aa0ab68189c7d48
oluwatobiloba <oluwateezzy03@gmail.com>
2022-10-19 08:06:40 -0700

    multiplication table

---
+++ 0x02-functions_nested_loops/100-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Return: 0 if success
+ */
+int main(void)
+{
+	print_times_table(3);
+	_putchar('\n');
+	print_times_table(5);
+	_putchar('\n');
+	print_times_table(98);
+	_putchar('\n');
+	print_times_table(12);
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_times_table - multiplication table
+ *
+ * Return: void
+ */
+void print_times_table(int n)
+{
+	int row, col, mult;
+
+	if (n >= 0 && n < 15)
+	{
+		for (row = 0; row <= n; row++)
+		{
+			for (col = 0; col <= n; col++)
+			{
+				mult = row * col;
+
+				if (col == 0)
+					_putchar('0');
+				else if (mult < 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(mult % 10 + 48);
+				}
+				else if (mult >= 10 && mult < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((mult / 10) + 48);
+					_putchar((mult % 10) + 48);
+				}
+				else if (mult > 99 && mult < 1000)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((mult / 100) + 48);
+					_putchar((mult / 10) % 10 + 48);
+					_putchar(mult % 10 + 48);
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
