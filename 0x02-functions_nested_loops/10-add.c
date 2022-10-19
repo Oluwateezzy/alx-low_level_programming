c6428f82f326cb3cea3ec0a2fb459ab4b84d85fb
oluwatobiloba <oluwateezzy03@gmail.com>
2022-10-19 01:46:54 -0700

    addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * add - add two numbers together
+ *
+ * Return: result
+ *
+ * @c: character
+ * 
+ * @c1: character
+ */
+int add(int c, int c1)
+{
+	int result;
+
+	result = c + c1;
+	return (result);
+}
