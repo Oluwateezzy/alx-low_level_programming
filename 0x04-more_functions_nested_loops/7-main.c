e93ccc16be9a9175f3cc7e6438febb341551effe
oluwatobiloba <oluwateezzy03@gmail.com>
2022-10-20 03:40:04 -0700

    diagonal

---
+++ 0x04-more_functions_nested_loops/7-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - entry point
+ * Return: always 0
+ */
+int main(void)
+{
+	print_diagonal(0);
+	print_diagonal(2);
+	print_diagonal(10);
+	print_diagonal(-4);
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_diagonal - diagonal with \
+ * @n: parameter
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			int j;
+
+			for (j = 0; j <= i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
