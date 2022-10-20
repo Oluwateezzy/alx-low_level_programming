1115a3797d3627488b691fb1fc1f9b5b10b843f2
oluwatobiloba <oluwateezzy03@gmail.com>
2022-10-20 16:38:15 -0700

    sum of fib

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -5,4 +5,22 @@
  */
 int main(void)
 {
+	int i;
+	unsigned long int first, second, next, sum;
+
+	sum = 0;
+	first = 1;
+	second = 2;
+	for (i = 1; i <= 50; ++i)
+	{
+		if (first < 4000000 && first % 2 == 0)
+		{
+			sum = sum + first;
+		}
+		next = first + second;
+		first = second;
+		second = next;
+	}
+	printf("%lu\n", sum);
+	return (0);
 }
