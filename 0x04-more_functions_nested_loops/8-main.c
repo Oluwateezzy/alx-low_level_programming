259a288927a8652019673868031db5ca164f289b
oluwatobiloba <oluwateezzy03@gmail.com>
2022-10-20 03:56:14 -0700

    square

---
+++ 0x04-more_functions_nested_loops/8-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - entry point
+ * Return: alway zero
+ */
+int main(void)
+{
+	print_square(2);
+	print_square(10);
+	print_square(0);
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_square - square with #
+ * Return: void
+ * @size: size of square
+ */
+void print_square(int size)
+{
+	if (size > 0)
+	{
+		int i;
+
+		for (i = 0; i < size; i++)
+		{
+			int j;
+
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
