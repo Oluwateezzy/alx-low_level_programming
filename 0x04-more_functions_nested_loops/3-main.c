60e99f445fe5519c90ba523c4126a99308c17633
oluwatobiloba <oluwateezzy03@gmail.com>
2022-10-20 02:22:30 -0700

    number speaks

---
+++ 0x04-more_functions_nested_loops/3-main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * main - entry point
+ * Return: 0 when success
+ */
+int main(void)
+{
+	print_numbers();
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * _putchar - function that return a character
+ * Return: character
+ */
+int _putchar(char c)
+{
+	return putchar(c);
+}
