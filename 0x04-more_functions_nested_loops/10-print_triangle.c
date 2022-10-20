43a62de04e0f42e0494f57535eef9ec0f6b98450
oluwatobiloba <oluwateezzy03@gmail.com>
2022-10-20 05:42:58 -0700

    betty style bug

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,21 +8,14 @@ void print_triangle(int size)
 {
 	if (size > 0)
 	{
-		int i;
-		
+		int i, j;
+
 		for (i = 1; i <= size; i++)
 		{
-			int j;
-
 			for (j = i; j < size; j++)
-			{
 				_putchar(' ');
-			}
-
 			for (j = 1; j <= i; j++)
-			{
 				_putchar('#');
-			}
 			_putchar('\n');
 		}
 	}
