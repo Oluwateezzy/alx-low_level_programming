15c5b4677462d755f881a1f81408ee6a2caf681f
oluwatobiloba <oluwateezzy03@gmail.com>
2022-10-20 05:31:08 -0700

    diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,19 +7,15 @@
  */
 void print_diagonal(int n)
 {
-	int i;
+	int i, j;
 
 	if (n > 0)
 	{
 		for (i = 0; i < n; i++)
 		{
-			int j;
-
 			for (j = 0; j < i; j++)
-			{
 				_putchar(' ');
-			}
-			_putchar('\\');
+			_putchar(92);
 			_putchar('\n');
 		}
 	}
