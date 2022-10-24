d7d0207e4ff758b73486ef7214072a87e57cb2b5
oluwatobiloba <oluwateezzy03@gmail.com>
2022-10-24 13:29:26 -0700

    half the lies they tell us aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * puts2 - print even character
+ * @str: string parameter
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
