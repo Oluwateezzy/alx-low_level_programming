76cbb417924b7e249de98183a1667589a260066c
oluwatobiloba <oluwateezzy03@gmail.com>
2022-10-24 10:48:57 -0700

    revese string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+/**
+ * print_rev - reverse a string
+ * Return: void
+ * @s: parameter
+ */
+void rev_string(char *s)
+{
+	int i;
+
+	i = strlen(s) - 1;
+
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
