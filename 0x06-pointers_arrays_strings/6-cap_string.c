cae67a68f286a0f053af976a88f514fa1d733871
oluwatobiloba <oluwateezzy03@gmail.com>
2022-10-27 12:10:04 -0700

    capitalize each word

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,28 +10,8 @@ char *cap_string(char *ch)
 
 	for (i = 0; ch[i] != '\0'; i++)
 	{
-		if (i == 0)
-		{
-			if (ch[i] >= 'a' && ch[i] <= 'z')
-				ch[i] = ch[i] - 32;
-			continue;
-		}
-		if (ch[i] == ' ')
-		{
-			++i;
-			if (ch[i] >= 'a' && ch[i] <= 'z')
-			{
-				ch[i] = ch[i] - 32;
-				continue;
-			}
-		}
-		else
-		{
-			if (ch[i] >= 'A' && ch[i] <= 'Z')
-			{
-				ch[i] = ch[i] + 32;
-			}
-		}
+		if (i == 0 || ch[i - 1] == ' ' && ch[i] >= 'a' && ch[i] <= 'z')
+			ch[i] = ch[i] - 32;
 	}
 	return (ch);
 }
