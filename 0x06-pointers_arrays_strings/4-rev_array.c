ecc03134774ca7ad145af9b8ee3cadb6d810b68c
oluwatobiloba <oluwateezzy03@gmail.com>
2022-10-27 05:27:35 -0700

    reverse an array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * reverse_array - reverse of an array
+ * @a: array parameter
+ * @n: length of array
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int count;
+
+	for (count = n - 1; count >= 0; count--)
+	{
+		if (count == 0)
+		{
+			printf(", ");
+		}
+		printf("%d", a[count]);
+	}
+	printf("\n");
+}
