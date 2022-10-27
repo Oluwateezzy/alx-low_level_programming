53c234886fea5fdca106c2334dab6986d05a3b53
oluwatobiloba <oluwateezzy03@gmail.com>
2022-10-27 04:27:29 -0700

    compare two string

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strcmp - compare two strings
+ * @s1: char parameter
+ * @s2: char parameter
+ * Return: int
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i, count, count2;
+
+	count = 0;
+	count2 = 0;
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+		count += s1[i];
+		count2 += s2[i];
+	}
+	return count - count2;
+}
