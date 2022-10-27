9c0a2151f23d6925a262b11b11b75024599dd088
oluwatobiloba <oluwateezzy03@gmail.com>
2022-10-27 06:57:16 -0700

    to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * string_toupper - string to uppercase
+ * @ch: string parameter
+ * Return: char
+ */
+char *string_toupper(char ch)
+{
+	int i;
+
+	for (i = 0; ch[i] != '\0'; i++)
+	{
+		if (ch[i] >= 97 && ch[i] <= 122)
+		{
+			ch[i] = ch[i] - 32;
+		}
+	}
+	return (ch);
+}
