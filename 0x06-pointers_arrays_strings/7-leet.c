7b825c1bc21492ea6020cee19092cb9e9a1f0c3e
oluwatobiloba <oluwateezzy03@gmail.com>
2022-10-27 13:09:12 -0700

    maybe

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,10 +7,10 @@
 char *leet(char *ch)
 {
 	int i, let;
-
 	int low_letters[] = {97, 101, 111, 116, 108};
 	int upp_letters[] = {65, 69, 79, 84, 76};
 	int numbers[] = {52, 51, 48, 55, 49};
+
 	for (i = 0; ch[i] != '\0'; i++)
 	{
 		for (let = 0; let < 5; let++)
