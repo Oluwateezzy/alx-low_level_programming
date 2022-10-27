9411262182c6be3791010034794d62bc17763667
oluwatobiloba <oluwateezzy03@gmail.com>
2022-10-27 07:52:10 -0700

    capitalize each word

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * cap_string - capitalize first leter of every string
+ * @ch: string of character
+ * Return: char
+ */
+char *cap_string(char *ch)
+{
+	int i;
+
+	for (i = 0; ch[i] != '\0'; i++)
+	{
+		if (i == 0)
+		{
+			if (ch[i] >= 'a' && ch[i] <= 'z')
+				ch[i] = ch[i] - 32;
+			continue;
+		}
+		if (ch[i] == ' ')
+		{
+			++i;
+			if (ch[i] >= 'a' && ch[i] <= 'z')
+			{
+				ch[i] = ch[i] - 32;
+				continue;
+			}
+		}
+		else
+		{
+			if (ch[i] >= 'A' && ch[i] <= 'Z')
+			{
+				ch[i] = ch[i] + 32;
+			}
+		}
+	}
+}
