127660ca00a94597e7e6412100c3313d02721bfd
oluwatobiloba <oluwateezzy03@gmail.com>
2022-10-27 14:18:46 -0700

    character is killing me

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,12 +6,24 @@
  */
 char *cap_string(char *ch)
 {
-	int i;
+	int i, count;
+	int sep_char = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125}
 
-	for (i = 0; ch[i] != '\0'; i++)
+	count = 0;
+	if (ch[count] >= 97 && ch[count] <= 122)
+		ch[count] = ch[count] - 32;
+	count++;
+	for (; ch[count] != '\0'; count++)
 	{
-		if ((i == 0 || ch[i - 1] == ' ') && ch[i] >= 'a' && ch[i] <= 'z')
-			ch[i] = ch[i] - 32;
+		for (i = 0; i < 13; i++)
+		{
+			if (ch[count] == sep_char[i])
+			{
+				if (ch[count + 1] >= 97 && ch[count + 1] <= 122)
+					ch[count + 1] = ch[count + 1] - 32;
+				break;
+			}
+		}
 	}
 	return (ch);
 }
