2b78eb545448bc6eba39478b2c7bfa889a418bb2
oluwatobiloba <oluwateezzy03@gmail.com>
2022-10-27 14:43:23 -0700

    advance task

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * rot13 - rot13 encryption
+ * @ch: string parameter
+ * Return: char
+ */
+char *rot13(char *ch)
+{
+	int i, j;
+
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	for (i = 0; ch[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (ch[i] == alphabet[j])
+			{
+				ch[i] = rot[j];
+				break;
+			}
+		}
+	}
+	return (ch);
+}
