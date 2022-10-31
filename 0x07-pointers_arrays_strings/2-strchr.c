c1daeb53b752ffaefdc4508a5b0fb8ca46e1ea77
oluwatobiloba <oluwateezzy03@gmail.com>
2022-10-31 16:55:30 -0700

    check character in string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,16 +7,16 @@
  */
 char *_strchr(char *s, char c)
 {
-	if (*s == c)
-	{
-		return (s);
-	}
 	for (; *s != '\0'; s++)
 	{
 		if (*s == c)
 		{
 			return (s);
 		}
+	}
+	if (*s == c)
+	{
+		return (s);
 	}
 	return (0);
 }
