2cbe0af7ad9d6e11a11c4d86b5a00c41574a37c6
oluwatobiloba <oluwateezzy03@gmail.com>
2022-11-01 02:49:25 -0700

    Length of substring

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,15 +7,21 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	int i, count;
+	int i, count, j;
 
 	count = 0;
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] == accept[i])
+		if (s[i] != 32)
 		{
-			count = count + 1;
+			for (j = 0; accept[j] != '\0'; j++)
+			{
+				if (s[i] == accept[j])
+					count++
+			}
 		}
+		else
+			return (count);
 	}
 	return (count);
 }
