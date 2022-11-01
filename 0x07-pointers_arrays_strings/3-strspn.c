a2e89f5122c54df64af82587d4814cd1cf01db96
oluwatobiloba <oluwateezzy03@gmail.com>
2022-10-31 17:27:47 -0700

    length of a prefix substring

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strspn - length of prefix substring
+ * @s: string character to be scanned
+ * @accept: string containing the list of characters to match
+ * Return: unsigned integer
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, count;
+
+	count = 0;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == accept[i])
+		{
+			count = count + 1;
+		}
+	}
+	return (count);
+}
