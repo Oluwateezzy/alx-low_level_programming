5bb093d2660c2e142f7f763675972591333c165c
oluwatobiloba <oluwateezzy03@gmail.com>
2022-11-01 03:18:19 -0700

    first matching character

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -7,14 +7,14 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-	int i, j;
+	int j;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (; *s != '\0'; s++)
 	{
-		for (j = 0; accept[j] = '\0'; j++)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
-			if (s[i] == accept[j])
-				return (s[i]);
+			if (*s == accept[j])
+				return (s);
 		}
 	}
 	return ('\0');
