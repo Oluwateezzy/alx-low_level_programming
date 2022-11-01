7235851c1b7573b3e0be1287ac8b92ad2a3cf7a4
oluwatobiloba <oluwateezzy03@gmail.com>
2022-11-01 05:23:22 -0700

    locate string

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * _strstr - locate substring
+ * @haystack: string to be scanned
+ * @needle: string to be searched
+ * Return: char
+ */
+char *_strstr(char *haystack, char *needle)
+{}
