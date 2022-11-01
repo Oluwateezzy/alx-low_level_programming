a6d48ef931252d21c7326ec38a330efb99eecd14
oluwatobiloba <oluwateezzy03@gmail.com>
2022-11-01 08:15:07 -0700

    sum of diagonal

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -8,15 +8,15 @@
  */
 void print_diagsums(int *a, int size)
 {
-	long long sum1, sum2;
+	int sum1, sum2;
 	int i;
 
 	sum1 = 0;
 	sum2 = 0;
 	for (i = 0; i < size; i++)
 	{
-		sum1 += a[i][i];
-		sum2 += a[i][n - i - 1];
+		sum1 += *(a + (size * i + i));
+		sum2 += *(a + (size * i + size - 1 - i));
 	}
-	printf("%ld, %ld", sum1, sum2);
+	printf("%d, %d", sum1, sum2);
 }
