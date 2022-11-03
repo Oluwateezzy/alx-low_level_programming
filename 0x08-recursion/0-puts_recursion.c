b8f6d7ad65e610bb8cce5421ea30f64228b4ce09
oluwatobiloba <oluwateezzy03@gmail.com>
2022-11-03 01:45:10 -0700

    print string recursively

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,9 +6,11 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s)
+	if (*s == '\0')
 	{
-		_puts_recursion(s - 1);
-		_putchar(s);
+		_putchar('\n');
+		return;
 	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
