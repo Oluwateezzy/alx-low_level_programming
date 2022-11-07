d9e79568208f64ee6671cdce28eac1053dad8a2f
oluwatobiloba <oluwateezzy03@gmail.com>
2022-11-07 10:33:57 -0800

    shell script

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * _putchar - function that returns a character
- *
+ * @c: character parameter
  * Return: character
  */
 int _putchar(char c)
 {
-	return putchar(c);
+	return (putchar(c));
 }
