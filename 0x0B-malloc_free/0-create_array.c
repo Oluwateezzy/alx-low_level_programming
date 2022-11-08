384567e1cb4ea97826f4a29f9c323dfe41f5ec70
oluwatobiloba <oluwateezzy03@gmail.com>
2022-11-08 13:14:36 -0800

    create an array of chars

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,7 +10,7 @@ char *create_array(unsigned int size, char c)
 {
 	if (size == 0)
 		return ('\0');
-	char *s = malloc(size * sizeof(char));
+	char *s = *malloc(size * sizeof(char));
 
 	if (*s == '\0')
 		return ('\0');
