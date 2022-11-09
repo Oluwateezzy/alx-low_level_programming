dda23932230c28ef7bdb42af276c1a65f4815633
oluwatobiloba <oluwateezzy03@gmail.com>
2022-11-08 19:33:15 -0800

    concatenate strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,22 +11,26 @@ char *str_concat(char *s1, char *s2)
 	unsigned int i, j, k;
 	char *str;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	for (i = 0; s1[i] != '\0'; i++)
 		;
 	for (j = 0; s2[j] != '\0'; j++)
 		;
 
-	str = (char *)malloc(sizeof(char) * ((i + 1) + (j + 1)));
+	str = (char *)malloc(sizeof(char) * (i + (j + 1)));
 
 	if(str == NULL)
 		return ('\0');
-	for (k = 0; k <= (i + 1); k++)
+	for (k = 0; k <= i; k++)
 	{
 		str[k] = s1[k];
 	}
-	for (k = 0; k <= (j + 1); k++)
+	for (i = 0; i <= j; i++, k++)
 	{
-		str[i + 1 + k] = s2[k];
+		str[k] = s2[i];
 	}
 	return (str);
 }
