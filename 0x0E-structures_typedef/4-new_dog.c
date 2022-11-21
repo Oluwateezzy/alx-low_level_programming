9efe3e8dfdca19333fdfadf0af525b9fdea84494
oluwatobiloba <oluwateezzy03@gmail.com>
2022-11-21 06:37:24 -0800

    create new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,7 +12,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *dog1 = malloc(sizeof(dog_t));
 
 	if (dog1 == NULL)
+	{
+		free(dog1);
 		return (NULL);
+	}
 	(*dog1).name = name;
 	(*dog1).age = age;
 	(*dog1).owner = owner;
