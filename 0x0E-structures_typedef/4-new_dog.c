6f8a9ea7f5e56538a00bb704705edfa17c45bce6
oluwatobiloba <oluwateezzy03@gmail.com>
2022-11-21 10:35:40 -0800

    copy string

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,35 +15,31 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (name == NULL || owner == NULL)
 		return ('\0');
-	for (x = 0; name[x] != '\0'; x++)
-		;
-	new_name = malloc(sizeof(char) * (x + 1));
-	if (new_name == NULL)
-	{
-		free(new_name);
-		return ('\0');
-	}
-	for (x = 0; owner[x] != '\0'; x++)
-		;
-	new_owner = malloc(sizeof(char) * (x + 1));
-	if (new_owner == NULL)
-	{
-		free(new_owner);
-		return ('\0');
-	}
 	dog1 = malloc(sizeof(dog_t));
 
 	if (dog1 == NULL)
 	{
-		free(dog1);
 		return (NULL);
 	}
+
+	for (x = 0; name[x] != '\0'; x++)
+		;
+	new_name = malloc(sizeof(char) * (x + 1));
+	if (new_name == NULL)
+		return ('\0');
 	for (i = 0; name[i] != '\0'; i++)
 		new_name[i] = name[i];
+	new_name[i] = '\0';
 	(*dog1).name = new_name;
 	(*dog1).age = age;
+	for (x = 0; owner[x] != '\0'; x++)
+		;
+	new_owner = malloc(sizeof(char) * (x + 1));
+	if (new_owner == NULL)
+		return ('\0');
 	for (i = 0; owner[i] != '\0'; i++)
 		new_owner[i] = owner[i];
+	new_owner[i] = '\0';
 	(*dog1).owner = new_owner;
 	return (dog1);
 }
