4d8a0a10f2b016457d787f61b1437d5491a72ee8
oluwatobiloba <oluwateezzy03@gmail.com>
2022-11-23 08:31:44 -0800

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+ * print_name - print name
+ * @name: name parameter
+ * @f: pointer to function parameter
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
