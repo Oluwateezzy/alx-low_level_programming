ed0f41ebfea9b56feec0b4beb58480222c0ba5c1
oluwatobiloba <oluwateezzy03@gmail.com>
2022-11-25 08:51:11 -0800

    alx commit

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,7 +3,8 @@
  * before_main - print before main function
  * Return: void
  */
-void __attribute__ ((constructor)) befor_main()
+void __attribute__ ((constructor)) before_main()
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
