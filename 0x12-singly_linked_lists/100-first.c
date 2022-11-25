66ae3dc81eee38d08eba5d9b27bb00235bc72afc
oluwatobiloba <oluwateezzy03@gmail.com>
2022-11-25 08:40:26 -0800

    committ

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 /**
  * before_main - print before main function
- * #constructor: constructor
  * Return: void
  */
-void before_main() __attribute__((constructor));
-void befor_main()
+void __attribute__ ((constructor)) befor_main()
 {
 	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
