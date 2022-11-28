3bacc11a0fd87ac0b6f509614ba6d9db170c89fa
oluwatobiloba <oluwateezzy03@gmail.com>
2022-11-28 01:49:36 -0800

    alx commit

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,17 +1,30 @@
 #include "calc.h"
 /**
- * main - entry point
- * @argc - length of argv
- * @argv - array
- * Return - int
+ * main - program entry point
+ * @argc: length of argument
+ * @argv: array of argument
+ * Return: integer
  */
 int main(int argc, char *argv)
 {
 	int arg1, arg2, result;
+	int (*operator)(int, int);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	
+	operator = get_op_func(argv[2]);
+	if (!operator)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	arg1 = atoi(argv[1]);
+	arg2 = atoi(argv[3]);
+
+	result = operator(arg1, arg2);
+	printf("%d\n", result);
+	return (0);
+}
