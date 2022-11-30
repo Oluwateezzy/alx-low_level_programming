c5ec4d76df6a170be3a8e7a85f385b7514ff9f39
oluwatobiloba <oluwateezzy03@gmail.com>
2022-11-30 03:04:03 -0800

    alx commit

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * pop_listint - delete the head node
+ * @head: head node
+ * Return: int
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *list;
+	int first_node;
+
+	if (*head == NULL)
+		return (0);
+	first_node = (*head)->n;
+	list = *head;
+	*head = (*head)->next;
+	return (first_node);
+}
