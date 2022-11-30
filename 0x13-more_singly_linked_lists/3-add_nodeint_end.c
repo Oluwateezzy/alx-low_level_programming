4ceef6a874ac19b160210ac2ccd3d8f3b0fe452a
oluwatobiloba <oluwateezzy03@gmail.com>
2022-11-30 01:54:19 -0800

    alx commit

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -7,17 +7,23 @@
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *new_node;
+	listint_t *new_node, *last_node;
 
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
-	while (*head)
-	{
-		*head = (*head)->next;
-	}
 	new_node->n = n;
 	new_node->next = NULL;
-	(*head)->next = new_node;
+	if (*head == NULL)
+		*head = new_node;
+	else
+	{
+		last_node = *head;
+		while (last_node->next != NULL)
+		{
+			last_node = last_node->next;
+		}
+		last_node->next = new_node;
+	}
 	return (*head);
 }
