55ce5ed263bc176e0be5e7727e383d26fad2c6fe
oluwatobiloba <oluwateezzy03@gmail.com>
2022-11-30 02:32:02 -0800

    alx commit

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_listint2 - free link list
+ * @head: head of list
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *list;
+
+	while (*head)
+	{
+		next = (*head)->next;
+		free(*head);
+		*head = next;
+	}
+}
