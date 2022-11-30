51f553bff9ff5776637d96c02f631876e2a89a06
oluwatobiloba <oluwateezzy03@gmail.com>
2022-11-30 01:00:54 -0800

    alx commit

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * add_nodeint_end - add node to the end of list
+ * @head: head of list
+ * @n: value
+ * Return: structure
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint *new_node;
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	while (*head)
+	{
+		*head = (*head)->next;
+	}
+	new_node->n = n;
+	new_node->next = NULL;
+	(*head)->next = new_node;
+	return (*head);
+}
