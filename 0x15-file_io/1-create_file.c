dbebab7fcd8f63957deb8ee5d41461d9c82c9413
oluwatobiloba <oluwateezzy03@gmail.com>
2022-12-12 07:24:27 -0800

    alx commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,13 +8,17 @@
 int create_file(const char *filename, char *text_content)
 {
 	ssize_t fd, wr;
+
 	if (filename == NULL)
 		return (-1);
 
 	fd = open(filename, O_WRONLY | O_CREAT | O_EXCL, 0600);
-	if ((fd == -1) && (EEXIST == errno))
+	if (fd == -1)
+		return (-1);
+	if (!text_content)
 	{
-		fd = open(filename, O_WRONLY);
+		close(fd);
+		return (1);
 	}
 	wr = write(fd, text_content, strlen(text_content));
 	if (wr == -1)
