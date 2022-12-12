ece99379190d6dadce6001dec0d6db1de8f2818a
oluwatobiloba <oluwateezzy03@gmail.com>
2022-12-12 07:08:17 -0800

    alx commit

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,6 +10,6 @@
 #include <errno.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
-int createe_file(const char *filename, char *text_content);
+int create_file(const char *filename, char *text_content);
 
 #endif
