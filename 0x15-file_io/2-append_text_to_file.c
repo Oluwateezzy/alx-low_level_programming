4841f43247c8a5987923729193d4f49346ddffda
oluwatobiloba <oluwateezzy03@gmail.com>
2022-12-12 07:59:10 -0800

    alx commit

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,7 +11,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_RDWR | O_APPEND, 0600);
 	if (fd == -1)
 		return (-1);
 	if (!text_content)
