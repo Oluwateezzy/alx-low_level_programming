fbc775561b9d5f6790ff6d32ec06c0b8b1bc11f7
oluwatobiloba <oluwateezzy03@gmail.com>
2022-12-12 07:54:54 -0800

    alx commit

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * append_text_to_file - appends text at the end of a file
+ * @filename: name of the file
+ * @text_content: file content
+ * Return: integer
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	ssize_t fd, wr;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	if (!text_content)
+	{
+		close(fd);
+		return (1);
+	}
+	wr = write(fd, text_content, strlen(text_content));
+	if (wr == -1)
+		return (-1);
+	close(fd);
+	return (1);
+}
