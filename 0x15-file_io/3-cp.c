61681098b2c1bae5a632c350553c3ad0cb2abac2
oluwatobiloba <oluwateezzy03@gmail.com>
2022-12-12 13:31:45 -0800

    alx commit

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,25 +7,39 @@
  */
 int main(int ac, char **av)
 {
-	ssize_t fd, wr;
+	ssize_t re, wr;
+	char buffer[1024], ch;
+	int file_from, file_to;
 
 	if (ac != 3)
 	{
-		fprintf(stderr, "Usage: cp file_from file_to\n");
+		printf("Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	fd = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0600);
-	if (!av[1])
+	file_from = open(av[1], O_RDONLY);
+	if (file_from == -1)
 	{
-		printf("Error: Can't write from file %s\n", av[1]);
+		printf("Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-	if (fd == -1)
+	file_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (file_to == -1)
 	{
 		printf("Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
-	wr = write(fd, av[1], strlen(av[1])
+	re = read(file_from, buffer, 1024);
+	if (re == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+	while (re)
+	{
+		wr = write(file_to, buffer. re);
+		if (wr == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+			exit(99);
+		}
 
-	return (1);
-}
