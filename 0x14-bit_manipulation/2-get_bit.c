31bfcf1030048111423ab5528daba95023e075fb
oluwatobiloba <oluwateezzy03@gmail.com>
2022-12-15 18:24:11 -0800

    return value at a given index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,8 +7,8 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	int rem, count;
-	unsigned long int n;
+	unsigned int rem, count;
+	unsigned long int prev;
 
 	count = 0;
 	while (n != 0)
