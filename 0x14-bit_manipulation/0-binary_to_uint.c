8f0c81be602a814d807b2d7fa67015fa9b40da01
oluwatobiloba <oluwateezzy03@gmail.com>
2022-12-15 16:22:12 -0800

    convert binary number to unsigned integer

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,26 +6,33 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int result, i;
+	unsigned int mult, res;
+	int i;
 
-	result = 1;
+	res = 0;
+	mult = 1;
 	if (b == NULL)
 		return (0);
 	for (i = 0; b[i] != '\0'; i++)
+		;
+	for (i--; i >= 0; i--)
 	{
-		int ch;
-		if (ch < '/' && ch > ':')
+		if (b[i] != '1' && b[i] != '0')
 			return (0);
-
-		ch = atoi(b[i]);
-		result = result + (ch * power(i));
+		if (b[i] == '1')
+		{
+			res += mult;
+		}
+		mult = mult << 1;
 	}
-	return (result);
+	return (res);
 }
-int power(int pow)
-{
-	if (pow != 0)
-		return (2 * power(pow - 1));
-	else
-		return (1);
-}
+/**
+ * int power(int pow)
+ * {
+ * if (pow != 0)
+ * return (2 * power(pow - 1));
+ * else
+ * return (1);
+ * }
+ */
