117ed9a7d8219898eb5f2408a43d1e8e79e9419f
oluwatobiloba <oluwateezzy03@gmail.com>
2022-12-15 18:19:30 -0800

    return value at a given index

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * get_bit - converts a binary number to an unsigned int
+ * @n: decimal number
+ * @index: index
+ * Return: int
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int rem, count;
+	unsigned long int n;
+
+	count = 0;
+	while (n != 0)
+	{
+		prev =  n;
+		n = n >> 1;
+		rem = prev - (n * 2);
+		if (index == count)
+			return (rem);
+		count++;
+	}
+	return (-1);
+}
