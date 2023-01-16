c116981d7155e4b181b62e86a23df02aa2454e22
Oluwatobiloba Oluwafunmilayo <53793687+Oluwateezzy@users.noreply.github.com>
2023-01-16 13:35:12 +0100

    Create 3-puts.c

---
+++ 0x18-dynamic_libraries/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts - put function
+ * @str: pointer parameter
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
