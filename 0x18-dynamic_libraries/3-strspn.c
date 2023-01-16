edb330156a7fafcec5af74620467d5c4f59b90a7
Oluwatobiloba Oluwafunmilayo <53793687+Oluwateezzy@users.noreply.github.com>
2023-01-16 13:37:08 +0100

    Create 3-strspn.c

---
+++ 0x18-dynamic_libraries/3-strspn.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strspn - length of prefix substring
+ * @s: string character to be scanned
+ * @accept: string containing the list of characters to match
+ * Return: unsigned integer
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, count, j;
+
+	count = 0;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] != 32)
+		{
+			for (j = 0; accept[j] != '\0'; j++)
+			{
+				if (s[i] == accept[j])
+					count++;
+			}
+		}
+		else
+			return (count);
+	}
+	return (count);
+}
