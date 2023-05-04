f22b91180124af1a4d7f84d8deacba301f11ce88
oluwatobiloba <oluwateezzy03@gmail.com>
2023-05-04 03:08:42 -0700

    print list

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - return the nth of a linked list
+ * @head: linked list
+ * @index: index of node
+ * Return; list
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *current = head;
+	size_t count = 0;
+
+	while (current != NULL)
+	{
+		if (count == index)
+		{
+			return (current);
+		}
+		count++;
+		current = current->next;
+	}
+	return (NULL);
+}
