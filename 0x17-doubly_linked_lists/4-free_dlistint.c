1a464fc8b542161c7419f4d44749f23092cb5739
oluwatobiloba <oluwateezzy03@gmail.com>
2023-05-04 02:50:41 -0700

    print list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_dlistint - free a double linked list
+ * @head: list
+ * Return: void
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current;
+
+	while (head != NULL)
+	{
+		current = head;
+		head = head->next;
+		free(current);
+	}
+}
