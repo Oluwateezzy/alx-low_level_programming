90c2cd4009caa24849dc8a6485f06d91f57f9166
oluwatobiloba <oluwateezzy03@gmail.com>
2023-05-04 09:28:33 -0700

    crackme

---
+++ 0x17-doubly_linked_lists/file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# uncompyle6 version 3.9.0
+# Python bytecode version base 3.4 (3310)
+# Decompiled from: Python 3.8.10 (default, Mar 13 2023, 10:26:41) 
+# [GCC 9.4.0]
+# Embedded file name: 100-crackme.py
+# Compiled at: 2017-01-05 16:44:37
+# Size of source mod 2**32: 195 bytes
+ok = 'Zen of Python'
+ok = ok + ' C'
+ok = ok[1:3] + ok[3] + ok[14] + ok[3] + ok[7:9] + ok[:6][::-1]
+print(ok)
+# okay decompiling crackme4.pyc
