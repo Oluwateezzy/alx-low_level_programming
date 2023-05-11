2373c7c73a1da90ac9ff46834c620a7f50bff67f
oluwatobiloba <oluwateezzy03@gmail.com>
2023-05-11 03:26:25 -0700

    linear search agorithm

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -8,15 +8,17 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
+	size_t i = 0;
+
 	if (array == NULL)
 	{
 		return (-1);
 	}
-	for (size_t i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
-		printf("Value checked array[%d] = [%d]", i, array[i]);
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		if (array[i] == value)
-			return (1);
+			return (i);
 	}
 	return (-1);
 }
