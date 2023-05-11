f2b800b0cd52d98be77758b077bdb8c63f150d39
oluwatobiloba <oluwateezzy03@gmail.com>
2023-05-11 04:12:53 -0700

    bin

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,39 @@
+#include "search_algos.h"
+/**
+ * linear_search - function that searches for a value in an array of integers
+ * @array: arrays of numbers
+ * @size: number elements in the array
+ * @value: is value to be search for
+ * Return: integer
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t i, high, low, mid;
+
+	high =  size - 1;
+	low = 0;
+
+	if (array == NULL)
+		return (-1);
+
+	printf("Searching in array: ");
+	for (i = low; i <= high; i++)
+	{
+		if (i == high)
+			printf("%d ", array[i]);
+		printf("%d, ", array[i]);
+        }
+	printf("\n");
+
+	while (low <= high)
+	{
+		mid =  (low + high) / 2;
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] < value)
+			low = mid + 1;
+		else
+			high = mid - 1;
+	}
+	return (-1);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,4 +4,5 @@
 #include <stdio.h>
 #include <string.h>
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
 #endif
