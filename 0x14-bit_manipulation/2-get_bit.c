72f8d23afea68848186ca775841caaea72f2679e
oluwatobiloba <oluwateezzy03@gmail.com>
2023-05-31 06:31:07 -0700

    update

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,18 +7,10 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int rem, count;
-	unsigned long int prev;
+	int bit;
 
-	count = 0;
-	while (n != 0)
-	{
-		prev =  n;
-		n = n >> 1;
-		rem = prev - (n * 2);
-		if (index == count)
-			return (rem);
-		count++;
-	}
-	return (-1);
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+	bit = (n >> index) & 1;
+	return (bit);
 }
--- 0x14-bit_manipulation/2-main.c
+++ 0x14-bit_manipulation/2-main.c
@@ -2,19 +2,15 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * main - check the code .
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    int n;
+	int n;
 
-    n = get_bit(1024, 10);
-    printf("%d\n", n);
-    n = get_bit(98, 1);
-    printf("%d\n", n);
-    n = get_bit(1024, 0);
-    printf("%d\n", n);
-    return (0);
+	n = get_bit(2048, 20);
+	printf("%d\n", n);
+	return (0);
 }
