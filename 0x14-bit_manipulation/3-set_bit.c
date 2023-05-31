0bb14d898636ea9af9352e3594ab9da4ebe490eb
oluwatobiloba <oluwateezzy03@gmail.com>
2023-05-31 06:45:50 -0700

    push

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * get_bit - converts a binary number to an unsigned int
+ * @n: decimal number
+ * @index: index
+ * Return: int
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mask;
+
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+	mask = 1UL << index;
+	*n |= mask;
+	return (1);
+}
