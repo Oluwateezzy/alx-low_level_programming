36419c86c41e9d47443f0ea1264579dbf237cc85
oluwatobiloba <oluwateezzy03@gmail.com>
2023-05-31 06:58:02 -0700

    fix

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * clear_bit - clear bit a specific index
+ * n: digit
+ * index: index
+ * Return: int
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mask;
+
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+	mask = 1UL << index;
+	mask = ~mask;
+	*n &= mask;
+	return (1);
+}
