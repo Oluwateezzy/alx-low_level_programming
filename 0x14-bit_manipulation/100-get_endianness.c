95712020022abe9b9998cc4b36d5afe866ee2dfe
oluwatobiloba <oluwateezzy03@gmail.com>
2023-05-31 11:35:22 -0700

    update

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
- * get_end - checks the endianness
+ * get_endianness - checks the endianness
  * Return: int
  */
 int get_endianness(void)
 {
 	unsigned int num = 1;
-	unsigned char* byte = (unsigned char*) &num;
+	unsigned char *byte = (unsigned char *) &num;
+
 	return ((int) (*byte));
 }
