4f2beba7133e9a2fea5e55220410bd969889aca1
oluwatobiloba <oluwateezzy03@gmail.com>
2023-06-13 08:54:42 -0700

    update

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,12 +12,15 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *current;
 
+	if (ht == NULL || key == NULL || strcmp(key, "") == 0)
+		return (NULL);
+
 	index = key_index((const unsigned char *)key, ht->size);
 	current = ht->array[index];
 	while (current != NULL)
 	{
 		if (strcmp(current->key, key) == 0)
-			return current->value;
+			return (current->value);
 		current = current->next;
 	}
 	return (NULL);
