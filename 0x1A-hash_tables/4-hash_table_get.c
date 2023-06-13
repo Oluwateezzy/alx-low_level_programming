a09706b031100ddf6ebc0f36dd2cfc81459c687d
oluwatobiloba <oluwateezzy03@gmail.com>
2023-06-13 08:46:41 -0700

    update

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include "hash_tables.h"
+/**
+ * hash_table_get - Retrieves the value associated with a key
+ * @ht: The hash table
+ * @key: The key string
+ * Return: char
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *current;
+
+	index = key_index((const unsigned char *)key, ht->size);
+	current = ht->array[index];
+	while (current != NULL)
+	{
+		if (strcmp(current->key, key) == 0)
+			return current->value;
+		current = current->next;
+	}
+	return (NULL);
+}
