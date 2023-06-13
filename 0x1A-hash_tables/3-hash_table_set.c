71cfa5fb4ea6a7dac47a68e0bab974d8582e23f9
oluwatobiloba <oluwateezzy03@gmail.com>
2023-06-13 08:39:16 -0700

    update

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,38 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * hash_table_set - Adds or updates a key/value pair in a hash table
+ * @ht: The hash table
+ * @key: The key string
+ * @value: The value string
+ * Return: int
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *new_node, *current;
+
+	if (ht == NULL || key == NULL || value == NULL || strcmp(key, "") == 0)
+		return (0);
+	index = key_index((const unsigned char *)key, ht->size);
+	current = ht->array[index];
+	while (current != NULL)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			free(current->value);
+			current->value = strdup(value);
+			return (1);
+		}
+		current = current->next;
+	}
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+	return (1);
+}
