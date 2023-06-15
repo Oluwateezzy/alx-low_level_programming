7ee97b2faeccb83d4940deb1f309115433ef5539
oluwatobiloba <oluwateezzy03@gmail.com>
2023-06-15 08:43:45 -0700

    update

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+/**
+ * add - add function
+ * @a: int
+ * @b: int
+ * Return: int
+ */
+int add(int a, int b) {
+    return a + b;
+}
+
+/**
+ * sub - sub
+ * @a: int
+ * @b: int
+ * Return: int
+ */
+int sub(int a, int b) {
+    return a - b;
+}
+
+/**
+ * mul - mul
+ * @a: a
+ * @b: int
+ * Return: int
+ */
+int mul(int a, int b) {
+    return a * b;
+}
+
+/**
+ * div - div
+ * @a: int
+ * @b: int
+ * Return: int
+ */
+int div(int a, int b) {
+    return a / b;
+}
+
+/**
+ * mod - mod
+ * @a: int
+ * @b: int
+ * Return: int
+ */
+int mod(int a, int b) {
+    return a % b;
+}
